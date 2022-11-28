c8e6ad1957d3f8fc0f16121c5fc4e5d960ee14d2
Tracie35 <kariukitracie@gmail.com>
2022-11-28 12:35:16 +0300

    done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -6,6 +6,12 @@ class BirdsController < ApplicationController
     render json: birds
   end
 
+  # POST /birds
+  def create
+    bird = Bird.create(bird_params)
+    render json: bird, status: :created
+  end
+
   # GET /birds/:id
   def show
     bird = Bird.find_by(id: params[:id])
@@ -14,6 +20,47 @@ class BirdsController < ApplicationController
     else
       render json: { error: "Bird not found" }, status: :not_found
     end
+  end
+
+  # PATCH /birds/:id
+  def update
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(bird_params)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
+  # PATCH /birds/:id/like
+  def increment_likes
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(likes: bird.likes + 1)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
+  #delete
+  def destroy
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.destroy
+      head: no_content
+      # render json: {}
+    else
+      render json: {error: 'Bird not found!'}, status: :not_found
+    end
+  end
+
+
+  private
+
+  def bird_params
+    params.permit(:name, :species, :likes)
   end
 
 end
